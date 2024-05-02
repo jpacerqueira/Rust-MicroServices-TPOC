5d9614d7bdf8cbfe92e671d188bc473b04a1bac9
Joao Pedro Afonso Cerqueira <114918447+jpacerqueira@users.noreply.github.com>
2024-05-02 16:58:27 +0100

    Create install-libc-latest.sh

---
+++ install-libc-latest.sh
@@ -0,0 +1,12 @@
+##!/usr/bin/env bash
+#
+## Install libc
+#
+wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
+tar -zxvf glibc-2.29.tar.gz
+mkdir glibc-2.29/build
+cd glibc-2.29/build
+../configure --prefix=/opt/glibc
+make
+make install
+#
